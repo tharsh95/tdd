7849942a84960219cadd9cc21f742a4fa53eeddc
Harsh Tiwari <harsh.tiwari@gmail.com>
2024-06-28 13:47:43 +0530

    Handle delimiter

--- sum.js
+++ sum.js
@@ -1,5 +1,14 @@
 function sum(...args){
-    return args.reduce((acc, curr) => acc + +(curr), 0);
+    let delimiter = "\n";
+    if (args[0].startsWith("//")) {
+        const delimiterEnd = args[0].indexOf("\n");
+        delimiter = args[0].substring(2, delimiterEnd);
+        args[0] = args[0].substring(delimiterEnd + 1);
+    }
+    const cleanedArray = args.map(el => el.split(delimiter)).flat();
+    return cleanedArray.reduce((acc, curr) => acc + Number(curr.trim()), 0);
+    // const cleanedArray = args.map(el=>el?.split("\n")).flat()
+    // return cleanedArray.reduce((acc, curr) => acc + +(curr), 0);
 }
-
+console.log(sum( "//;\n1;2"))
 module.exports = sum
--- sum.test.js
+++ sum.test.js
@@ -1,20 +1,20 @@
 const sum = require('./sum')
 
-test("Adds two numbers", () => {
-  expect(sum(1, 2)).toBe(3);
-});
 test("Adds two strings", () => {
   expect(sum("1", "2")).toBe(3);
 });
 test("Adds two strings separated with a new line", () => {
-  expect(sum("1\n", "2")).toBe(3);
+  expect(sum("1\n2")).toBe(3);
 });
 test("Passed one argument", () => {
-  expect(sum("")).toBe(0);
+  expect(sum("1")).toBe(1);
 });
 test("Passed Empty string", () => {
   expect(sum("")).toBe(0);
 });
 test("Handle mutliple elements", () => {
   expect(sum("1","5","6")).toBe(12);
+});
+test("Handle delimiter", () => {
+  expect(sum( "//;\n1;2")).toBe(3);
 });
