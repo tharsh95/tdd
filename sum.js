936782340d3464bc5939e986204ad42e29f154ee
Harsh Tiwari <harsh.tiwari@gmail.com>
2024-06-28 14:51:31 +0530

    refactor

--- sum.js
+++ sum.js
@@ -1,14 +1,17 @@
 function sum(...args){
-    let delimiter = "\n";
+  const cleanedArray = arrayClean(args)
+    return cleanedArray.reduce((sum, curr) => sum + +(curr), 0);
+}
+const arrayClean =(args)=>{
+
+    let delimiter="\n"
     if (args[0].startsWith("//")) {
-        const delimiterEnd = args[0].indexOf("\n");
-        delimiter = args[0].substring(2, delimiterEnd);
-        args[0] = args[0].substring(delimiterEnd + 1);
+        const lastIndex = args[0].indexOf("\n");
+        delimiter = args[0].substring(2, lastIndex);
+        args[0] = args[0].substring(lastIndex + 1);
     }
     const cleanedArray = args.map(el => el.split(delimiter)).flat();
-    return cleanedArray.reduce((acc, curr) => acc + Number(curr.trim()), 0);
-    // const cleanedArray = args.map(el=>el?.split("\n")).flat()
-    // return cleanedArray.reduce((acc, curr) => acc + +(curr), 0);
+    return cleanedArray;
 }
-console.log(sum( "//;\n1;2"))
+sum("1","2")
 module.exports = sum
